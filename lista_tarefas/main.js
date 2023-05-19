d2b38fcf6b4c5d7292f046fbf720aa5735ce9dbc
aluno <cassiodemaz@gmail.com>
2023-05-19 17:01:09 -0300

    lista de tarefas

---
+++ lista_tarefas/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('header button').click(function(e){
+        $('form').slideDown();
+    })
+
+    $('#botao_cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novaTarefa= $('#tarefa_nova').val();
+        const tarefaAtualizada = $('<li style = "display: none"></li>');
+        $(`<p${novaTarefa}></p>`).appendTo(tarefaAtualizada);
+        $(`
+            <li id="lista">
+                ${novaTarefa}
+            </li>
+        `).appendTo(tarefaAtualizada);
+        $(tarefaAtualizada).appendTo('ul');
+        $(tarefaAtualizada).fadeIn(100);
+        $('#tarefa_nova').val('');
+
+        $('li').click(function(){
+            $(this).css("text-decoration","line-through");
+        })
+    })
+    });
+
