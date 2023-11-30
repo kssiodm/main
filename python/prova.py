9844232c2fc491616bcf51374f836ddd8477d836
kssiodm <cassiodemaz@gmail.com>
2023-11-30 17:02:38 -0300

    update python

--- python/prova.py
+++
@@ -1,5 +0,0 @@
-%%writefile notas.csv
-matricula;questao1;questao2;questao3;questao4;questao5;questao6;questao7;questao8;questao9;questao10
-30;a;d;e;a;c;c;e;b;d;d
-33;b;c;a;e;c;b;a;e;c;a
-35;e;c;e;c;b;e;d;a;d;e
