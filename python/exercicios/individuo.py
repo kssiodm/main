99e844e0731538fa1b1e6e0e6a8599c0b1e171b6
kssiodm <cassiodemaz@gmail.com>
2023-12-02 15:14:10 -0300

    tentando

--- python/exercicios/individuo.py
+++ python/exercicios/individuo.py
@@ -1,8 +1,19 @@
 from humano import Humano
 
-class Main():
+# Criando uma pessoa
+pessoa1 = Humano(nome="Alice", idade=18, peso=60, altura=160)
 
-    def main():
+# Exibindo informações iniciais
+print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
 
-        cidadao1 = Humano('carlos', 6 , 75, 1,32)
-        cidadao1.mostrarDados()
+# Envelhecendo a pessoa por 3 anos
+pessoa1.envelhecer(3)
+
+# Engordando a pessoa em 5 quilos
+pessoa1.engordar(5)
+
+# Emagrecendo a pessoa em 2 quilos
+pessoa1.emagrecer(2)
+
+# Exibindo informações finais
+print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
