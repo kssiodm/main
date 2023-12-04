27a0697846883a13217d13cadf1b2465582bc3d0
kssiodm <cassiodemaz@gmail.com>
2023-12-04 17:04:50 -0300

    updade exercecios

--- python/exercicios/individuo.py
+++ python/exercicios/individuo.py
@@ -1,19 +1,19 @@
 from humano import Humano
 
-# Criando uma pessoa
-pessoa1 = Humano(nome="Alice", idade=18, peso=60, altura=160)
+class Principal():
 
-# Exibindo informações iniciais
-print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
+    def main():
+        pessoa1 = Humano(nome="Alice", idade=15, peso=60, altura=1.60)
 
-# Envelhecendo a pessoa por 3 anos
-pessoa1.envelhecer(3)
 
-# Engordando a pessoa em 5 quilos
-pessoa1.engordar(5)
+        print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
 
-# Emagrecendo a pessoa em 2 quilos
-pessoa1.emagrecer(2)
+        pessoa1.envelhecer(3)
 
-# Exibindo informações finais
-print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
+        pessoa1.engordar(5)
+
+        pessoa1.emagrecer(2)
+
+        print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Altura: {pessoa1.altura}")
+    if __name__ == '__main__':
+        main()
